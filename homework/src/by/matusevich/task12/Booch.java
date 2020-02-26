775125ed595d5e12077cd5122f78957ce3bbba40
sergeymatusevich <serg.matusevich@gmail.com>
2020-02-26 22:16:57 +0300

    fixed

--- homework/src/by/matusevich/task12/Booch.java
+++ homework/src/by/matusevich/task12/Booch.java
@@ -2,21 +2,21 @@ package by.matusevich.task12;
 
 public class Booch {
     public static void main(String[] args) {
-        int x = 12345678;
+        int x = 9876;
         int digit;
         int i = 0;
-        int y = 0;
+        int y = 1;
         int count = 0;
-        System.out.println(x);
+        System.out.println("исходный х= "+x);
         while (x != 0) {
             y = y * 10 + x % 10;
             x /= 10;
             count++;
 
         }
-        System.out.println(y);
+        System.out.println("перевернутый х= "+y);
 
-        while (y != 0) {
+        while (y != 1) {
             digit = y % 10;
             y /= 10;
             System.out.print(digit);
