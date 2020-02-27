9cb9f2b4f5dbcdba66aa31ed09ee7716ff66b8dd
sergeymatusevich <serg.matusevich@gmail.com>
2020-02-27 14:34:09 +0300

    it works

--- homework/src/by/matusevich/task12/Booch.java
+++ homework/src/by/matusevich/task12/Booch.java
@@ -2,28 +2,24 @@ package by.matusevich.task12;
 
 public class Booch {
     public static void main(String[] args) {
-        int x = 9876;
+        int x = 100000;
         int digit;
         int i = 0;
         int y = 1;
         int count = 0;
         System.out.println("исходный х= "+x);
-        while (x != 0) {
+        for (;x != 0;x/=10) {
             y = y * 10 + x % 10;
-            x /= 10;
             count++;
-
         }
         System.out.println("перевернутый х= "+y);
 
-        while (y != 1) {
+        for (;y != 1;y/=10) {
             digit = y % 10;
-            y /= 10;
             System.out.print(digit);
             i++;
             if ((count-i)%3==0)
                 System.out.print(" ");
-
         }
 
     }
---
+++ homework/src/by/matusevich/task14/NumberMax.java
@@ -0,0 +1,32 @@
+package by.matusevich.task14;
+
+import java.util.Random;
+
+public class NumberMax {
+    public static void main(String[] args) {
+
+        int marks[] = new int[20];
+        Random random = new Random();
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = random.nextInt(10);
+            marks[i]++; //оценки от 1 до 10
+            System.out.println("[" + i + "]" + " " + marks[i]);
+        }
+        int maxMark = marks[0];
+
+        for (int i = 0; i < marks.length; i++) {
+            if (maxMark < marks[i]) {
+                maxMark = marks[i];
+                //System.out.println("number of max mark");
+                //System.out.println(maxMark);
+
+            }
+        }
+        for (int i = 0; i < marks.length; i++) {
+            if (maxMark == marks[i]){
+                System.out.print("Number of max mark =");
+                System.out.println(i);
+        }}
+        System.out.println("Maxmark = " + maxMark);
+    }
+}
