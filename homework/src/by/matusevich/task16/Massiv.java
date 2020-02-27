102a0248904a81b03e707dc69b336aa9c738077b
sergeymatusevich <serg.matusevich@gmail.com>
2020-02-27 23:54:24 +0300

    works

---
+++ homework/src/by/matusevich/task16/Massiv.java
@@ -0,0 +1,27 @@
+package by.matusevich.task16;
+
+import java.util.Random;
+
+public class Massiv {
+    public static void main(String[] args) {
+        int temp = 0;
+        int arr[] = new int[10];
+        //Random random=new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+            System.out.print("[" + i + "]" + " " + arr[i]+" ");
+
+        }
+        System.out.println();
+            for (int j = 0; j < arr.length/2+1; j++) {
+
+                temp = arr[j];
+                arr[j] = arr[arr.length-1-j];
+                arr[arr.length-1-j] = temp;
+            }
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.print("[" + i + "]" + " " + arr[i]+" ");
+        }
+    }
+}
