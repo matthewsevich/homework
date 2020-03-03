919f12870960bfd80a86bbcb62aa5c8a4bdd5799
sergeymatusevich <serg.matusevich@gmail.com>
2020-03-03 19:27:56 +0300

    task17 with 1 cycle

---
+++ homework/src/by/matusevich/task17/NewMatrix.java
@@ -0,0 +1,34 @@
+package by.matusevich.task17;
+
+public class NewMatrix {
+    public static void main(String[] args) {
+        int N = 7;
+        int m[][] = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            int from, to;
+            if (i < N / 2) {
+                from = i;
+                to = N - i;
+            }
+            else {
+                from = N - i - 1;
+                to = i + 1;
+            }
+            for (int j = 0; j < N; j++) {
+                if (j >= from && j < to) {
+                    m[i][j] = 1;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+
+                System.out.print(m[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
