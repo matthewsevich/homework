5e97f7eb34671d13c00a5bcc9e70f4de938b74ad
sergeymatusevich <serg.matusevich@gmail.com>
2020-03-04 00:14:50 +0300

    string 50+ mins stringbuilder 2secs

---
+++ homework/src/by/matusevich/task21/StringBuilderAppend.java
@@ -0,0 +1,12 @@
+package by.matusevich.task21;
+
+public class StringBuilderAppend {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder
+                = new StringBuilder("");
+        for (int i = 0; i < 100_0000; i++)
+
+        stringBuilder.append("съешь ещё этих мягких французских булок, да выпей чаю");
+    }
+}
+
---
+++ homework/src/by/matusevich/task21/StringString.java
@@ -0,0 +1,9 @@
+package by.matusevich.task21;
+
+public class StringString {
+    public static void main(String[] args) {
+        String str=" ";
+        for (int i=0;i<100_000;i++)
+            str+="съешь ещё этих мягких французских булок, да выпей чаю";
+    }
+}
