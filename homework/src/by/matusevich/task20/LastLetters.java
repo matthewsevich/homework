daa1635de858f0322472a32cc3af49164c43fda5
sergeymatusevich <serg.matusevich@gmail.com>
2020-03-05 18:52:53 +0300

    now works for last letter

--- homework/src/by/matusevich/task20/LastLetters.java
+++ homework/src/by/matusevich/task20/LastLetters.java
@@ -2,20 +2,22 @@ package by.matusevich.task20;
 
 public class LastLetters {
     public static void main(String[] args) {
-        String str = "asd,asd. asd :asd;asd-asd?";
+        String str = "Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence";
         char symbol;
 
         for (int i = 0; i < str.length(); i++) {
             symbol = str.charAt(i);
-            if ((symbol == ',') || (symbol == '.')
+            if (((symbol == ',') || (symbol == '.')
                     || (symbol == '?') || (symbol == '!')
                     || (symbol == ':') || (symbol == ';')
-                    || (symbol == '-') || (symbol == ' ')) {
-                if (Character.isLetter(str.charAt(i - 1)))
+                    || (symbol == '-') || (symbol == ' '))
+                    && (Character.isLetter(str.charAt(i - 1))))
 
-                    System.out.print(str.charAt(i - 1) + " ");
+                System.out.print(str.charAt(i - 1) + " ");
 
-            }
         }
+        if (Character.isLetter(str.charAt(str.length() - 1)))
+            System.out.print(str.charAt(str.length() - 1));
     }
 }
+
