89b435d7b2abaab46f37d97ed47c8f6471c12426
sergeymatusevich <serg.matusevich@gmail.com>
2020-03-07 01:11:37 +0300

    USER INTERFACE

--- homework/src/by/matusevich/task03/NewLastDigit.java
+++ homework/src/by/matusevich/task03/NewLastDigit.java
@@ -1,11 +1,16 @@
 package by.matusevich.task03;
 
+import java.util.Scanner;
+
 public class NewLastDigit {
     public static void main(String[] args) {
-
-        int x = (int) (Math.random()*1000);
-        String s = (x % 10 == 7) ? "у ИКС = " +x+" последняя - цифра 7" : "у ИКС = " +x+"  последняя - цифра не 7";
+        int input=readNumber("Введите число, последнюю цифру которого вы хотите проверить на 7");
+        String s = (input % 10 == 7) ? "у числа = " +input+" последняя - цифра 7" : "у числа = " +input+"  последняя - цифра не 7";
         System.out.println(s);
-
+    }
+    static int readNumber(String msg){
+        System.out.println(msg);
+        Scanner scanner=new Scanner(System.in);
+        return scanner.nextInt();
     }
 }
