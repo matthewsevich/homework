88697a7368c0469c59c947de163b93e8e78ab37d
sergeymatusevich <serg.matusevich@gmail.com>
2020-03-07 00:44:40 +0300

    USER INTERFACE

--- homework/src/by/matusevich/task02/WeekConverter.java
+++ homework/src/by/matusevich/task02/WeekConverter.java
@@ -1,24 +1,35 @@
 package by.matusevich.task02;
 
+import java.util.Scanner;
+
 public class WeekConverter {
 
     public static void main(String[] args) {
-        int s = 111111111;
-        int sec;
-        int m, min;
-        int h, hour;
-        int d, day;
-        int w;
+        int input = readNumber("Insert amount of seconds you want to convert to weeks: ");
 
-        sec = s % 60;
-        m = (s - sec) / 60;
-        min = m % 60;
-        h = (m - min) / 60;
-        hour = h % 24;
-        d = (h - hour) / 24;
-        day = d % 7;
-        w = (d - day) / 7;
-        System.out.println(w + " недель " + day + " дней " + hour + " часов " + min + " минут " + sec + " секунд");
+        if (input < 0)
+            System.out.println(" ERROR");
 
+            int sec;
+            int m, min;
+            int h, hour;
+            int d, day;
+            int w;
+
+            sec = input % 60;
+            m = (input - sec) / 60;
+            min = m % 60;
+            h = (m - min) / 60;
+            hour = h % 24;
+            d = (h - hour) / 24;
+            day = d % 7;
+            w = (d - day) / 7;
+            System.out.println(w + " недель " + day + " дней " + hour + " часов " + min + " минут " + sec + " секунд");
+    }
+
+    static int readNumber(String msg) {
+        System.out.println(msg);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
     }
 }
