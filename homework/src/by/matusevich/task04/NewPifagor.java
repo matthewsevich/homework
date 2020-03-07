f85538b36e1428c9028e1f5ee6443b77c92f16d9
sergeymatusevich <serg.matusevich@gmail.com>
2020-03-07 11:31:52 +0300

    USER INTERFACE

--- homework/src/by/matusevich/task04/NewPifagor.java
+++ homework/src/by/matusevich/task04/NewPifagor.java
@@ -1,16 +1,25 @@
 package by.matusevich.task04;
 
+
+import java.util.Scanner;
+
 public class NewPifagor {
     public static void main(String[] args) {
-        int a =3;
-        int b = 4;
-        int r = 6;
-        System.out.println("Сторона а " + a);
-        System.out.println("Сторона б " + b);
-        System.out.println("Радиус r " + r);
+        int a=readNumber("Введите первую сторону квадратного отверстия");
+        int b=readNumber("Введите вторую сторону квадратного отверстия");
+        int r=readNumber("Введите радиус круглой картонки");
 
-        String s = (((a * a) + (b * b)) <= ((r * 2)*(r*2))) ? "Можно" : "Нельзя";
+        System.out.println("Сторона a: " + a);
+        System.out.println("Сторона b: " + b);
+        System.out.println("Радиус r: " + r);
+
+        String s = (((a * a) + (b * b)) <= ((r * 2)*(r*2))) ? "Можно закрыть отверстие" : "Нельзя закрыть отверстие";
         System.out.println(s);
 
+    }
+    static int readNumber(String msg){
+        System.out.println(msg);
+        Scanner scanner=new Scanner(System.in);
+        return scanner.nextInt();
     }
 }
