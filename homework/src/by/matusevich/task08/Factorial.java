2f936fab676c83f3680621074035dc225028bd45
sergeymatusevich <serg.matusevich@gmail.com>
2020-03-28 11:19:02 +0300

    Fixing error in Task8

--- homework/src/by/matusevich/task08/Factorial.java
+++ homework/src/by/matusevich/task08/Factorial.java
@@ -2,18 +2,21 @@ package by.matusevich.task08;
 
 import by.matusevich.task01.Utility;
 
+import java.math.BigInteger;
+
 class Factorial {
     public static void main(String[] args) {
 
         int input = Utility.readNumber("Введите число факториал которого нужно вычислить ");
-        long fac = 1;
+        BigInteger fac = BigInteger.ONE;
         int i = 1;
 
         while (i <= input) {
-            fac *= i;
+            fac = fac.multiply(BigInteger.valueOf(i));
             i++;
         }
         System.out.println("Для числа = " + (i - 1) + " факториал равен " + fac);
-        }
     }
+}
+
 
