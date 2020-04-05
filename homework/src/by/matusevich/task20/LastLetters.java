3bf973c99c8d48d2f88e22b8d6a192ebc7f66557
sergeymatusevich <serg.matusevich@gmail.com>
2020-04-05 16:50:42 +0300

    fixed error in task 20

--- homework/src/by/matusevich/task20/LastLetters.java
+++ homework/src/by/matusevich/task20/LastLetters.java
@@ -6,20 +6,21 @@ public class LastLetters {
     public static void main(String[] args) {
         String str = Utility.readString("Введите строку для вывода последних букв из каждого слова");
         char symbol;
-
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 1; i < str.length(); i++) {
             symbol = str.charAt(i);
             if (((symbol == ',') || (symbol == '.')
                     || (symbol == '?') || (symbol == '!')
                     || (symbol == ':') || (symbol == ';')
                     || (symbol == '-') || (symbol == ' '))
-                    && (Character.isLetter(str.charAt(i - 1))))
-
+                    && (Character.isLetter(str.charAt(i - 1)))) {
                 System.out.print(str.charAt(i - 1) + " ");
 
+            }
         }
-        if (Character.isLetter(str.charAt(str.length() - 1)))
+        if (Character.isLetter(str.charAt(str.length() - 1))) {
             System.out.print(str.charAt(str.length() - 1));
+
+        }
     }
 
 
