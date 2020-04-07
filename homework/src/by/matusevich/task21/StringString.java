27d0b8375f21d36533339d1572b58001085188ed
sergeymatusevich <serg.matusevich@gmail.com>
2020-04-07 16:17:55 +0300

    Fixed task21(added time measurements)

--- homework/src/by/matusevich/task21/StringString.java
+++ homework/src/by/matusevich/task21/StringString.java
@@ -2,8 +2,20 @@ package by.matusevich.task21;
 
 public class StringString {
     public static void main(String[] args) {
-        String str=" ";
-        for (int i=0;i<100_000;i++)
-            str+="съешь ещё этих мягких французских булок, да выпей чаю";
+        String str = " ";
+        long t1 = System.currentTimeMillis();
+        for (int i = 0; i < 10_000; i++)
+            str += "съешь ещё этих мягких французских булок, да выпей чаю";
+        System.out.println("10_000 конкатенаций String " + (System.currentTimeMillis() - t1) + " милисекунд");
+
+        StringBuilder stringBuilder
+                = new StringBuilder("");
+        long t2 = System.currentTimeMillis();
+
+        for (int i = 0; i < 10_000; i++)
+
+            stringBuilder.append("съешь ещё этих мягких французских булок, да выпей чаю");
+        System.out.println("10_000 конкатенаций StringBuilder " + (System.currentTimeMillis() - t2) + " милисекунд");
+
     }
 }
